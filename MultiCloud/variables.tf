a6ed2c072fdbb76c2c4586a342125659fb2f37ce
Hemanth Sai Balasada <balasadahemanth@gmail.com>
2025-10-08 14:16:11 +0530

    Update variables.tf

--- MultiCloud/variables.tf
+++ MultiCloud/variables.tf
@@ -1,1 +1,21 @@
+variable "project" {
+  default = "multi_cloud"
+}
 
+variable "location" {
+  default = "East US"
+}
+variable "tags" {
+  type = map(string)
+  default = {
+    Environment = "dev"
+    Project     = "multi_cloud"
+  }
+}
+
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources into"
+  type        = string
+  default     = "us-east-1" 
+}
