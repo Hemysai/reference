7b809c179753870558e950acf90c4d038b41fac8
Hemanth Sai Balasada <balasadahemanth@gmail.com>
2025-10-08 14:24:15 +0530

    Update main.tf

--- MultiCloud/main.tf
+++ MultiCloud/main.tf
@@ -1,1 +1,13 @@
+module "azure_blob" {
+  source   = "../../azure/blob_storage"
+  project  = var.project
+  location = var.location
+  tags     = var.tags
+}
+
+module "s3" {
+  source  = "../../aws/s3"
+  project = var.project
+  tags    = var.tags
+}
 
